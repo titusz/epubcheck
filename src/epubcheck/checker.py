ce78eb5152c254dc7872989fa7a3e672dbd9057b
titusz <tp@py7.de>
2016-04-03 14:20:59 +0200

    add main EpubCheck class

---
+++ src/epubcheck/checker.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+import json
+import subprocess
+from epubcheck import const as c
+from epubcheck.models import Message, Checker, Meta
+
+
+class EpubCheck:
+    """Wraps an epubcheck task and provides results as native python objects.
+
+    :param str infile: path to epubfile to be checked
+    :param str lang: set language for generated messages
+    :param str profile: name of epubcheck profule to use
+    :param bool autorun: wether to run the checking process on instantiation.
+    """
+
+    DEFAULT = 'default'
+    DICT = 'dict'
+    EDUPUB = 'edupub'
+    IDX = 'idx'
+    PREVIEW = 'preview'
+
+    def __init__(self, infile, lang='en', profile=DEFAULT, autorun=True):
+        self.infile = infile
+        self.lang = lang
+        self.profile = profile
+        self.autorun = autorun
+
+        self._stdout = None
+        self._stderr = None
+        self._returncode = None
+        self._messages = None
+        self.result_data = None
+        self.valid = None
+        self.checker = None
+        self.meta = None
+        self.messages = None
+        if autorun:
+            self.run()
+
+    def run(self):
+        lopt = '-Duser.language={}'.format(self.lang)
+        cmd = [
+            c.JAVA, lopt, '-jar', c.EPUBCHECK, self.infile,
+            '-q', '--profile', self.profile, '--json', '-',
+        ]
+
+        with open(os.devnull, "w") as devnull:
+            process = subprocess.Popen(
+                cmd, stdout=subprocess.PIPE, stderr=devnull,
+            )
+
+            self._stdout, self._stderr = process.communicate()
+            self._returncode = process.returncode
+        self.valid = True if self._returncode == 0 else False
+        self.result_data = json.loads(self._stdout)
+        self.checker = Checker.from_data(self.result_data)
+        self.meta = Meta.from_data(self.result_data)
+        self.messages = Message.from_data(self.result_data)
---
+++ tests/test_checker.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from epubcheck.checker import EpubCheck
+from epubcheck import samples
+
+
+def test_epubcheck_valid():
+    check = EpubCheck(samples.EPUB3_VALID)
+    assert check.valid is True
+    assert check.checker.nError == 0
+
+
+def test_epubcheck_invalid():
+    check = EpubCheck(samples.EPUB3_INVALID)
+    assert check.valid is False
+    assert check.checker.nError == 2
