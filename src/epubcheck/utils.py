01723f126170b0e202b5310f6cab23f589ecc702
titusz <tp@py7.de>
2016-04-03 14:20:27 +0200

    add util to generate sample output

--- src/epubcheck/utils.py
+++ src/epubcheck/utils.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 import os
 import subprocess
-import locale
+
+from epubcheck import samples
 from epubcheck import const as c
+from epubcheck.checker import EpubCheck
 
 
 def java_version():
@@ -24,17 +26,17 @@ def epubcheck_help():
     :return unicode: helptext from epubcheck.jar
     """
 
-    tc = locale.getdefaultlocale()[1]
+    # tc = locale.getdefaultlocale()[1]
 
     with open(os.devnull, "w") as devnull:
         p = subprocess.Popen(
-            [c.JAVA, '-jar', c.EPUBCHECK, '-h'],
+            [c.JAVA, '-Duser.language=en', '-jar', c.EPUBCHECK, '-h'],
             stdout=subprocess.PIPE,
             stderr=devnull,
         )
         result = p.communicate()[0]
 
-    return result.decode(tc)
+    return result.decode()
 
 
 def epubcheck_version():
@@ -45,5 +47,16 @@ def epubcheck_version():
     return epubcheck_help().splitlines()[0]
 
 
+def generate_sample_json():
+    """Generate sample json data for testing"""
+
+    check = EpubCheck(samples.EPUB3_VALID)
+    with open(samples.RESULT_VALID, 'wb') as jsonfile:
+        jsonfile.write(check._stdout)
+
+    check = EpubCheck(samples.EPUB3_INVALID)
+    with open(samples.RESULT_INVALID, 'wb') as jsonfile:
+        jsonfile.write(check._stdout)
+
 if __name__ == "__main__":
-    print(epubcheck_help())
+    generate_sample_json()
