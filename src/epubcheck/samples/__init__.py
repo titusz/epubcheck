474e14337302d7faebfef46a9165c8fdfbc80ed9
titusz <tp@py7.de>
2016-04-03 14:17:52 +0200

    add more sample data

--- src/epubcheck/samples/__init__.py
+++ src/epubcheck/samples/__init__.py
@@ -4,4 +4,6 @@ from os import path
 ROOT = path.abspath(path.dirname(__file__))
 
 EPUB3_VALID = path.join(ROOT, 'childrens-literature.epub')
-EPUB3_INVALID = path.join(ROOT, 'childrens-literature-invalid.epub')
+EPUB3_INVALID = path.join(ROOT, 'invalid.epub')
+RESULT_VALID = path.join(ROOT, 'result_valid.json')
+RESULT_INVALID = path.join(ROOT, 'result_invalid.json')
