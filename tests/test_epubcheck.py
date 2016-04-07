9dbd1495fcc7460099d57a978f52586b4c6dbab7
titusz <tp@py7.de>
2016-04-08 00:02:54 +0200

    add first tests for cli.main

--- tests/test_epubcheck.py
+++ tests/test_epubcheck.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import epubcheck
 from epubcheck import samples
 from epubcheck.cli import main
-
-
-def test_main():
-    assert main([]) == 0
 
 
 def test_valid():
@@ -14,3 +11,20 @@ def test_valid():
 
 def test_invalid():
     assert not epubcheck.validate(samples.EPUB3_INVALID)
+
+
+def test_main_valid(capsys):
+    argv = [samples.EPUB3_VALID]
+    exit_code = main(argv)
+    out, err = capsys.readouterr()
+    assert 'VALID' in out
+    assert 'INVALID' not in out
+    assert exit_code == 0
+
+
+def test_main_invalid(capsys):
+    argv = [samples.EPUB3_INVALID]
+    exit_code = main(argv)
+    out, err = capsys.readouterr()
+    assert 'INVALID' in out
+    assert exit_code == 1
