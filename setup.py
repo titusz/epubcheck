8e3c9d51abd6e3d4ac70909255f4119f0a8f89c5
titusz <tp@py7.de>
2016-04-10 21:08:32 +0200

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if sys.version.startswith('2.6'):
 
 setup(
     name='epubcheck',
-    version='0.2.0',
+    version='0.3.0',
     license='BSD',
     description='Python epubcheck wrapper',
     long_description='%s\n%s' % (
