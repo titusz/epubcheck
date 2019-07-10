d74b2d06b9de8f8290b088d24ddc9a45e769238d
Sean Quinn <swquinn@gmail.com>
2019-07-10 13:07:23 -0400

    Allow for openjdk Java versions
    In addition to official Java distributables, allow for openjdk versions
    as part of the Java version check test. This mostly came up as part of
    this test failing for Python 3.7 on Xenial distros, where the OpenJDK
    is used rather than the official Oracle Java release.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,7 +9,11 @@ TEST_DIR = abspath(dirname(samples.__file__))
 
 
 def test_utils_java_version():
-    assert utils.java_version().startswith('java version')
+    java_version = utils.java_version()
+    accepted_versions = ['java version', 'openjdk version']
+
+    actual = [java_version.startswith(v) for v in accepted_versions]
+    assert any(actual)
 
 
 def test_epubcheck_help():
